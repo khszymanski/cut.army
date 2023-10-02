780145a81e3d752e8c4b8a169cfe11fa4f837b8f
K. H. Szymański <h.szymanski@khs.company>
2023-10-02 19:33:54 +0200

    dzialajacy form

--- src/Controller/UrlsController.php
+++ src/Controller/UrlsController.php
@@ -2,6 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Url;
+use App\Form\UrlFormType;
+use App\Repository\UrlRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -9,9 +14,49 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class UrlsController extends AbstractController
 {
-    #[Route('/', name: 'app_urls_index')]
-    public function index(): Response
+    private $urlRepository;
+    private $em;
+
+    public function __construct(UrlRepository $urlRepository, EntityManagerInterface $em)
     {
-        return $this->render('index.html.twig');
+        $this->urlRepository = $urlRepository;
+        $this->em = $em;
+    }
+
+    #[Route('/', name: 'app_urls_index')]
+    public function index(Request $request): Response
+    {
+        $url = new Url();
+        $form = $this->createForm(UrlFormType::class, $url);
+
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() && $form->isValid()){
+            $newURL = $form->getData();
+            
+
+            $url = $newURL->getUrl();
+            $createdAt = \DateTimeImmutable::createFromFormat('Y-m-d H:i:s', date('Y-m-d H:i:s'));
+
+            $newURL->setUrl($url);
+            $newURL->setCreatedAt($createdAt);
+
+            $this->em->persist($newURL);
+            $this->em->flush();
+
+            $id = $newURL->getId();
+
+            return $this->render('index.html.twig', [
+                'form' => $form,
+                'newURL' => $id
+            ]);
+        }
+
+
+        return $this->render('index.html.twig', [
+            'form' => $form
+        ]);
+
+        
     }
 }
--- src/Form/UrlFormType.php
+++ src/Form/UrlFormType.php
@@ -6,15 +6,20 @@ use App\Entity\Url;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class UrlFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('url')
-            ->add('createdAt')
-        ;
+            ->add('url', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control w-100',
+                    'placeholder' => 'https://example.com/'
+                ],
+                'label' => false
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
