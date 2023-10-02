f6af86fdd69ed8e0ebf7aaccd584e80cb9a85929
K. H. Szymański <h.szymanski@khs.company>
2023-10-02 18:40:39 +0200

    home view template

--- src/Controller/UrlsController.php
+++ src/Controller/UrlsController.php
@@ -2,18 +2,16 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class UrlsController extends AbstractController
 {
-    #[Route('/urls', name: 'app_urls')]
-    public function index(): JsonResponse
+    #[Route('/', name: 'app_urls_index')]
+    public function index(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/UrlsController.php',
-        ]);
+        return $this->render('index.html.twig');
     }
 }
